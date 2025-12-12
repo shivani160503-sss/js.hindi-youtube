e29fe523e7f7d6efaca154ce231db150f1fbb3c5
shivani160503-sss <shivanipatel160503@gmail.com>
2025-12-12 06:13:39 +0000

    done

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,8 +1,14 @@
 const accountID = 122334
 let accountEmail = "sp.ppatel05@gmail.com"
 var accountPassword = "12345"
+accountCity = "Jaipur"
 
 
 // accountId = 2
 
-console.log(accountId);
+accountEmail = "hsc@hc.com"
+accountPassord = "322434564"
+accountCity = "Bengaluru"
+
+console.log(accountID);
+console.table([accountID,accountEmail,accountPassword , accountCity])
