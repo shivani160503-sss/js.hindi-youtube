d959f4bb9459fdba392103fba690d68808630853
shivani160503-sss <shivanipatel160503@gmail.com>
2025-12-12 06:44:53 +0000

    done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict";//tret all JS code as newedr version 
+
+// alert( 3 + 3) we are using node.js , not browser
+
+console.log( 3+3);
+        //code readability should be high
+console.log("Shivani");
+
+let name = "shivani"
+let age = 22
+let isLoggedIn = false
+
+// number => 2 to power 34
+// bigint 
+// sting => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+
+// object
+
+console.log(typeof "shivani");
+console.log(typeof undefined);
+console.log(typeof null);
