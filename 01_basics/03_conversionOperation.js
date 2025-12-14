64088c15942c928615bcd4fa0a24c80962318627
shivani160503-sss <shivanipatel160503@gmail.com>
2025-12-14 06:52:46 +0000

    done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -4,20 +4,33 @@ let score = "33abc";
 
 
 
-console.log(typeof score);
-console.log(typeof score);
+// console.log(typeof score);
+// console.log(typeof score);
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 // "33" => 33
 // "33abc" => NaN
 // true => 1; false => 0
 
-let isLogged =1
+let isLogged = ""
 
 
 
 let booleanIsLogged = Boolean(isLogged)
-console
+// console.log(booleanIsLogged);
+
+// 1 => true; 0 => false
+// "" => false
+// "shivani" => true
+// " " => true
+// [] => true
+// {} => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
+console.log(stringNumber);
