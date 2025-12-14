91fb30fe1e21b238a22989ddf8a9cdb729904b1e
shivani160503-sss <shivanipatel160503@gmail.com>
2025-12-14 06:13:00 +0000

    done

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,23 @@
+let score = "33abc";
+
+
+
+
+
+console.log(typeof score);
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLogged =1
+
+
+
+let booleanIsLogged = Boolean(isLogged)
+console
