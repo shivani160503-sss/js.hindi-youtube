6e7c0b73cf4dac3c7f136c8fd8cf36a3119a40e6
shivani160503-sss <shivanipatel160503@gmail.com>
2025-12-14 07:21:46 +0000

    done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -32,5 +32,40 @@ let booleanIsLogged = Boolean(isLogged)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(typeof stringNumber);
-console.log(stringNumber);
+// console.log(typeof stringNumber);
+// console.log(stringNumber);
+
+
+
+//******************Operation*****************
+
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+
+// console.log(2+2); //4
+// console.log(2-2); //0
+// console.log(2*2); //4
+// console.log(2/2); //1
+// console.log(2%3); 
+// console.log(2**2); 
+
+// +=, -=, *=, /=, %=
+
+let str1 = "Hello ";
+let str2 = "World";
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+
+// console.log("1"+2); //"12"
+// console.log(1+"2"); //"12"
+// console.log("1"+2+2); //"122"
+// console.log(1+2+"2"); //"32"
+
+//console.log(3 + 4) * 5 % 3 ); //2
+
+console.log(+true);
