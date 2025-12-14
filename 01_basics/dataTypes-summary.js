15380e9ead228dfb82969f06e078fc5c194c54e0
shivani160503-sss <shivanipatel160503@gmail.com>
2025-12-14 15:25:08 +0000

    done

--- 01_basics/dataTypes-summary.js
+++ 01_basics/dataTypes-summary.js
@@ -17,6 +17,23 @@ const anotherId = Symbol('12345');
 const bigNumber = 1234567890123456789012345678901234567890n;
 console.log( bigNumber); // bigint
 
+
+
 // Reference Data Types (non-primitive data types)
 
 // Object , Array , Functions
+
+
+
+const heros = ["Shivani" , "Ankita" , "Pooja" , "Neha"];
+
+let myObej = {
+    name : "Shivani",
+    age : 24, 
+}
+
+function myFunction(){
+    console.log("Hello User");
+}
+
+myFunctionr();
