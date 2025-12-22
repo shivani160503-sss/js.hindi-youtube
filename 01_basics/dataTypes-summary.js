d94f56b3d0058cffae666fd5bd4d541f9fee5dab
shivani160503-sss <shivanipatel160503@gmail.com>
2025-12-22 13:22:55 +0000

    done

--- 01_basics/dataTypes-summary.js
+++ 01_basics/dataTypes-summary.js
@@ -2,20 +2,20 @@
 
 // 7 types : Strings , Numbers, BigInt, Boolean, Undefined, Null, Symbol
 
-const score = 100 ;
-const scoreVAlue = 100.3;
+// const score = 100 ;
+// const scoreVAlue = 100.3;
 
-const isLoggedIn = false;
-const outsideTemp = null;
-let userEmail; // undefined
+// const isLoggedIn = false;
+// const outsideTemp = null;
+// let userEmail; // undefined
 
-const id = Symbol('12345');
-const anotherId = Symbol('12345');
+// const id = Symbol('12345');
+// const anotherId = Symbol('12345');
 
- console.log(id === anotherId); // false
+//  console.log(id === anotherId); // false
 
-const bigNumber = 1234567890123456789012345678901234567890n;
-console.log( bigNumber); // bigint
+// const bigNumber = 1234567890123456789012345678901234567890n;
+// console.log( bigNumber); // bigint
 
 
 
@@ -25,15 +25,36 @@ console.log( bigNumber); // bigint
 
 
 
-const heros = ["Shivani" , "Ankita" , "Pooja" , "Neha"];
+// const heros = ["Shivani" , "Ankita" , "Pooja" , "Neha"];
 
-let myObej = {
-    name : "Shivani",
-    age : 24, 
+// let myObej = {
+//     name : "Shivani",
+//     age : 24, 
+// }
+
+// function myFunction(){
+//     console.log("Hello User");
+// }
+
+// myFunction();
+
+
+//+++++++++++++++++++++++++++++++++
+
+// Stack ( Primitive data types )  vs Heap ( Reference data types )
+
+let myYoutubeChannel = "CodeWithShivani";
+let anotherChannel = myYoutubeChannel;
+anotherChannel = "CodeWithShivi";
+
+console.log({myYoutubeChannel , anotherChannel});
+
+
+let userOne = {
+    email : "shivani@gmail.com",
 }
+let userTwo = userOne;
+userTwo.email = "shivi@gmail.com"
 
-function myFunction(){
-    console.log("Hello User");
-}
-
-myFunction();
+console.log(userOne.email);
+console.log(userTwo.email);
