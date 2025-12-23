4956a77f6e48e85523032da2ccace996477bc7e6
shivani160503-sss <shivanipatel160503@gmail.com>
2025-12-23 12:03:15 +0000

    done

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,29 @@
+const name = "Vani";
+const repoCount = 50;
+
+// console.log(name + repoCount + " repositories");
+
+// console.log(`Hello ${name} , you have ${repoCount} repositories`);
+
+
+const gameName = "chess";
+
+console.log(`My favourite game is ${gameName}`);
+console.log(gameName.toUpperCase());
+
+console.log(gameName.toLowerCase());
+
+console.log(gameName.slice(0,3));
+
+console.log(gameName.length);
+
+console.log(gameName.includes("ch"));
+
+console.log(gameName.includes("zz"));
+
+console.log(gameName.replace("chess" , "carrom"));
+
+
+const newStringOne = "       Shivani       ";
+console.log(newStringOne);
+console.log(newStringOne.trim()); // removes spaces from starting and ending
